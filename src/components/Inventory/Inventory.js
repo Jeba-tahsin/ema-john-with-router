b650cc57df9a7254443ab8c84d8cc0e7b8fec11d
Jeba-tahsin <jebatahsin500@.com>
2021-10-01 12:53:10 +0600

    'added'

---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Inventory = () => {
+    return (
+        <div>
+            <h5>hola gola cola pola</h5>
+        </div>
+    );
+};
+
+export default Inventory;
